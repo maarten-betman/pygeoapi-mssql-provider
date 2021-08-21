5ba390a224d26ef553c395e67a025eaf224301d7
maarten-betman <maarten.betman@boskalis.com>
2021-08-21 11:18:38 +0800

    Commit files into git

---
+++ pygeoapi_mssql_provider/mssql.py
@@ -0,0 +1,9 @@
+from vendor import BaseProvider
+
+
+class DataBaseConnection:
+    def __init__(self, conn_dic, table, context="query"):
+        """"""
+
+class MsSqlProvider(BaseProvider):
+    pass
